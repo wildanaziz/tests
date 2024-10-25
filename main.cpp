363ae4cf7d8602364a94cf8380a6101781b076a5
wildanaziz <danitatadanwildan@gmail.com>
2024-10-25 20:32:52 +0700

    kode saya

---
+++ main.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int a[4] = {4,10,12,15};
+
+    for (int i = 0; i < a[i]; i++)
+    {
+        if (a[i]%2 == 0 )
+        {
+            cout << a[i] << endl;
+        }
+        
+    }
+
+    return 0;
+}
